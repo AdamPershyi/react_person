14e844d5d947562e9f75cfa174a538512f3a04b6
AdamPershyi <en0tadam@gmail.com>
2025-08-28 19:15:15 +0300

    task solution 02

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,17 @@
 import './Person.scss';
 // export const Person = ({ person }) => ();
 export const Person = ({ person }) => {
+  const showAge = typeof person.age !== 'undefined' && person.age !== null;
+
   const getPartnerText = () => {
     if (!person.isMarried) {
       return 'I am not married';
     }
 
-    const partnerTitle = person.sex === 'm' ? 'wife' : 'husband';
+    // eslint-disable-next-line prettier/prettier
+    const partnerTitle = person.sex === 'm'
+      ? 'wife'
+      : 'husband';
 
     return `${person.partnerName} is my ${partnerTitle}`;
   };
@@ -14,7 +19,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      {showAge && <p className="Person__age">{`I am ${person.age}`}</p>}
       <p className="Person__partner">{getPartnerText()}</p>
     </section>
   );
